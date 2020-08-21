06329d319348fcdd2b9466a8e8c84e686dfe6629
LeviScott13 <levi.sutton2013@gmail.com>
2020-08-20 22:41:59 -0400

    Added work experience

--- js/myScript.js
+++ js/myScript.js
@@ -4,6 +4,9 @@ var images = new Array();
 var eduParalax = document.getElementById("eduParalax");
 var images2 = new Array();
 
+var expParalax = document.getElementById("expParalax");
+var images3 = new Array();
+
 images[0] = 'paralax2';
 images[1] = 'pingPong';
 images[2] = 'uga';
@@ -13,9 +16,14 @@ images2[0] = 'paralax3';
 images2[1] = 'sgsc';
 images2[2] = 'vsu';
 
+images3[0] = 'paralax4';
+images3[1] = 'bball';
+images3[2] = 'adidas';
+
 aboutParalax.className = images[0];
 var p = images.length;
 var p2 = images2.length;
+var p3 = images3.length;
 
 var switchImage;
 var switchImage2;
@@ -25,12 +33,13 @@ const interval = setInterval(function changImage(){
     switchImage = Math.round(Math.random() * (p-1));
     switchImage2 = Math.round(Math.random() * (p2-1));
     switchImage3 = Math.round(Math.random() * (p3-1));
+
     aboutParalax.className = images[switchImage];
     eduParalax.className = images2[switchImage2];
     expParalax.className = images3[switchImage3];
 },5000);
 
-/************************* Tab Content *************************/
+/************************* Menu *************************/
 function openNav() {
     document.getElementById("myNav").style.width = "60%";
 }
@@ -38,6 +47,7 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+/************************* Tab Content *************************/
 
 function openCity(evt, schoolName) {
 var i, tabcontent, tablinks;
@@ -53,3 +63,21 @@ document.getElementById(schoolName).style.display = "block";
 evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+
+
+/************************* Tab Content2 *************************/
+function openExp(evt, expName) {
+var j, tabcontent2, tablinks2;
+tabcontent2 = document.getElementsByClassName("tabcontent2");
+for (j = 0; j < tabcontent2.length; j++) {
+    tabcontent2[j].style.display = "none";
+}
+tablinks2 = document.getElementsByClassName("tablinks2");
+for (j = 0; j < tablinks2.length; j++) {
+    tablinks2[j].className = tablinks2[j].className.replace(" active", "");
+}
+document.getElementById(expName).style.display = "block";
+evt.currentTarget.className += " active";
+}
+document.getElementById("expDefaultOpen").click();
